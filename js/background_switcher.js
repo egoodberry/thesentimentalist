e7330bbf4e6231fd55f8cd2867df4b89bb20dfe8
Evan Goodberry <egoodberry@gmail.com>
2013-04-21 11:21:12 -0400

    Cross-browser compatibility, etc.

--- js/background_switcher.js
+++ js/background_switcher.js
@@ -4,7 +4,7 @@
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     var imageIndex = random(1, 4);
-    var backgroundImage = "url(/images/home-" + imageIndex + ".jpg)";
-    $('#home').css("background-image", backgroundImage);
+    var backgroundImage = "url(images/home-" + imageIndex + ".jpg)";
+    $('#home .inner-content').css("background-image", backgroundImage);
   };
 })();
