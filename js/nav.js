f340ade3d5668313cb11b06b537ada67cd96c2fe
Evan Goodberry <egoodberry@gmail.com>
2013-04-23 21:35:22 -0400

    Fix scroll to top link.

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,19 @@
 (function() {
   App.setUpNavigation = function() {
+    // smooth scrolling nav
     $("#header nav").visualNav({
       selectedAppliedTo: "a",
-      selectedClass: "current",
-      changed: function(e, el) {
-        var atTop = (el.length === 0);
-        if (atTop) {
-          $('#back-to-top').fadeOut();
-        }
-        else {
-          $('#back-to-top').fadeIn();
-        }
+      selectedClass: "current"
+    });
+
+    // back to top link
+    $(window).bind('scroll', function() {
+      var atTop = ($(window).scrollTop() < 450);
+      if (atTop) {
+        $('#back-to-top').fadeOut();
+      }
+      else {
+        $('#back-to-top').fadeIn();
       }
     });
   };
