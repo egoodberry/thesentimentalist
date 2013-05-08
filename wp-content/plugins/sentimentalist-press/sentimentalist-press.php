113d8955b6654b894c84a7252e2f440ab13fba77
Evan Goodberry <egoodberry@gmail.com>
2013-05-07 20:27:27 -0400

    Convert to WordPress.

---
+++ wp-content/plugins/sentimentalist-press/sentimentalist-press.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: The Sentimentalist Press
+Plugin URI: http://www.thesentimentalistatl.com/#press
+Description: Press plugin for The Sentimentalist
+Version: 1.0
+Author: Evan Goodberry
+Author URI: http://www.someweather.com
+*/
+?>
